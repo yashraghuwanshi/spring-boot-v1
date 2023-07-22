ebb17659f9ac905e1564c0c3481c777a464fd06a
yashraghuwanshi <yash.sanjay.raghuwanshi@example.com>
2023-07-22 23:45:05 +0530

    First Commit

---
+++ src/test/java/com/example/springboot/tutorials/SpringBootRestApiH2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiH2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
